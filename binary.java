ee96d59b39705deea55a45e403cbb580f834c4c0
Mohita Chaudhary <mohitachaudhary02@gmail.com>
2018-07-18 00:10:51 +0530

    Add files via upload

---
+++ binary.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class binary
+{
+    public static void main(String a[])
+    {
+        int arr[]={1,2,4,7,3,9};
+        System.out.println(Arrays.binarySearch(arr,2));
+        System.out.println(Arrays.binarySearch(arr,9));
+    }
+}
